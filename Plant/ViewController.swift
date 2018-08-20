51cb93922d8a3cb3440c69c4e66214c6f842e01b
Olivia <obrown917@gmail.com>
2018-08-19 20:31:02 -0700

    Create project

---
+++ Plant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plant
+//
+//  Created by Olivia Brown on 8/19/18.
+//  Copyright © 2018 Olivia Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
