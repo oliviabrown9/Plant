25f6bc492b7c0798ce0eb07b7e53bd7d9bd8bcb1
Olivia <obrown917@gmail.com>
2018-08-19 20:48:14 -0700

    Remove storyboard

---
+++ Plant/ServingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Plant
+//
+//  Created by Olivia Brown on 8/19/18.
+//  Copyright © 2018 Olivia Brown. All rights reserved.
+//
+
+import UIKit
+
+class ServingsViewController: UIViewController {
+
+}
+
